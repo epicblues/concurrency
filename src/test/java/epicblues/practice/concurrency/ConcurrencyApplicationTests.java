240a4070e7632cae4257e267de80e96ef9923471
Minsung Kim <epicblue@hanmail.net>
2022-10-08 21:28:58 +0900

    chore : initial commit

---
+++ src/test/java/epicblues/practice/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package epicblues.practice.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
